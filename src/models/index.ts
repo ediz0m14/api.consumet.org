fd6769f3c0d051b98e0da319b5e71bafc1c50cf9
ediz0m14 <84308165+ediz0m14@users.noreply.github.com>
2026-03-01 14:54:21 -0500

    Update index.ts

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,11 @@
-import { IBookProviderParams, LibgenBook } from './types';
+export * from './types';
 
-export { IBookProviderParams, LibgenBook };
+import { IVideo } from './types';
+
+export abstract class VideoExtractor {
+  protected abstract serverName: string;
+  protected abstract sources: IVideo[];
+  protected readonly client = require('axios');
+
+  abstract extract(videoUrl: URL): Promise<IVideo[]>;
+}
